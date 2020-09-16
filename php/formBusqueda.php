f48d85d6cd6534ac17be713605384ed03d5b9df6
duvan128 <duvan1282017@outlook.com>
2020-09-16 17:27:34 -0500

    prueba3

---
+++ php/formBusqueda.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buscar</title>
+</head>
+<body>
+
+    <form action="buscar.php" method="post">
+        
+    <input type="text" name="codigo" placeholder="Ingrese codigo a buscar">
+    <input type="submit" name="name" value="Buscar">
+    </form>
+    
+</body>
+</html>
